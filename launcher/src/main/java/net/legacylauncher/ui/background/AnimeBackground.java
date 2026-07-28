890c40c16e47b76254853b0e0603457ef5e1fdd8
TZ <rozakadm@gmail.com>
2026-07-28 12:42:15 +0700

    fix: add missing IOException import in AnimeBackground

--- launcher/src/main/java/net/legacylauncher/ui/background/AnimeBackground.java
+++ launcher/src/main/java/net/legacylauncher/ui/background/AnimeBackground.java
@@ -14,6 +14,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
